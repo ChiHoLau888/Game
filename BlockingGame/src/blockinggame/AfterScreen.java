ad6f07f633ac9be8d60c07ab7e5ebeae513f8b8b
apinayanxyz <akanenthirarasa@gmail.com>
2023-01-15 00:03:04 +0000

    test

--- BlockingGame/src/blockinggame/AfterScreen.java
+++ BlockingGame/src/blockinggame/AfterScreen.java
@@ -5,10 +5,42 @@
  */
 package blockinggame;
 
+import static blockinggame.Game.SCREENHEIGHT;
+import static blockinggame.Game.SCREENWIDTH;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+
 /**
  *
  * @author abina
  */
 public class AfterScreen {
-    
+
+    private final Rectangle coverScreen;
+    private final Btn returnBtn;
+    private Group root;
+    private Scene scene;
+    private boolean ifWin;
+
+    public AfterScreen(int length, int height, boolean ifWin, Group root, Scene scene) {
+        coverScreen = new Rectangle(0, 0, length, height);
+        coverScreen.setOpacity(0.7);
+        returnBtn = new Btn();
+        returnBtn.setX((length / 2) - 75);
+        returnBtn.setY((height / 2) + 200);
+        this.ifWin = ifWin;
+        this.root = root;
+        this.scene = scene;
+    }
+
+    public void endGame() {
+        root.getChildren().add(coverScreen);
+        root.getChildren().add(returnBtn.getBtn());
+        if (ifWin) {
+
+        } else {
+
+        }
+    }
 }
